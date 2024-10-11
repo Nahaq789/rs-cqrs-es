003073980d77184d1a0eac9bc62d93c3b8a8fa76
Ran Mouri <naha022677@icloud.com>
2024-10-11 23:30:53 +0900

    fix: Price

---
+++ modules/command/domain/src/value_object/price.rs
@@ -0,0 +1,48 @@
+use std::fmt::{Display, Formatter};
+
+/// 金額のクラスです
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Price {
+  price: i32,
+}
+
+/// 金額エラーおのクラスです
+#[derive(Debug, Clone)]
+struct PriceError;
+
+impl Display for PriceError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "Price failed to validate")
+  }
+}
+
+impl Display for Price {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", self.price)
+  }
+}
+
+impl TryFrom<i32> for Price {
+  type Error = PriceError;
+
+  /// 実質的なコンストラクタです
+  fn try_from(value: i32) -> Result<Self, Self::Error> {
+    if value < 0 {
+      Err(PriceError)?
+    };
+    Ok(Self::new(value))
+  }
+}
+
+impl Price {
+  /// プライベートコンストラクタです
+  fn new(price: i32) -> Self { Self { price } }
+
+  /// 引数で受け取った値で足し算
+  pub fn add(self, value: i32) -> Result<Self, PriceError> {
+    if value < 0 {
+      Err(PriceError)?
+    }
+    Ok(Self::new(self.price + value))
+  }
+}
