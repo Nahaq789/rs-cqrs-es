380cee1f55bb393f57fd5a29ad31e0cdcccca5a0
Nahaq789 <naha022677@icloud.com>
2024-10-14 21:33:30 +0900

    fix: add Quantity Error

--- modules/command/domain/src/order/order_error.rs
+++ modules/command/domain/src/order/order_error.rs
@@ -1,13 +1,14 @@
 use crate::value_object::price::PriceError;
 use thiserror;
 use thiserror::Error;
+use crate::value_object::quantity::QuantityError;
 
 /// 注文のエラーです
 #[derive(Debug, Error)]
 pub enum OrderError {
-  #[error("Quantity must be greater than or equal to 0 {0:?}")]
-  InvalidQuantityError(u16),
+    #[error("Quantity must be greater than or equal to 0 {0:?}")]
+    InvalidQuantityError(#[from] QuantityError),
 
-  #[error("Price must be at least 1 {0:?}")]
-  InvalidPriceError(#[from] PriceError),
+    #[error("Price must be at least 1 {0:?}")]
+    InvalidPriceError(#[from] PriceError),
 }
