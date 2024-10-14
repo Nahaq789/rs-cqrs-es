7a94d7cea430f81824a8f5b8f853b2afebf99200
Nahaq789 <naha022677@icloud.com>
2024-10-14 20:20:53 +0900

    test: Quantity

--- modules/command/domain/src/value_object/quantity.rs
+++ modules/command/domain/src/value_object/quantity.rs
@@ -1,9 +1,19 @@
 use std::fmt::{Display, Formatter};
+use thiserror::Error;
 
 /// 数量を表すValueObjectです
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Quantity {
-  quantity: u16,
+  quantity: i32,
+}
+
+#[derive(Error, Debug, Clone)]
+pub struct QuantityError;
+
+impl Display for QuantityError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "Quantity failed to validate")
+  }
 }
 
 impl Display for Quantity {
@@ -12,9 +22,14 @@ impl Display for Quantity {
   }
 }
 
-impl From<u16> for Quantity {
-  fn from(value: u16) -> Self {
-    Self::new(value)
+impl TryFrom<i32> for Quantity {
+  type Error = QuantityError;
+
+  fn try_from(value: i32) -> Result<Self, Self::Error> {
+    if value <= 0 {
+      Err(QuantityError)?
+    };
+    Ok(Quantity::new(value))
   }
 }
 
@@ -28,7 +43,7 @@ impl Quantity {
   /// # Return
   ///
   /// * `Quantity`
-  pub fn new(quantity: u16) -> Self {
+  fn new(quantity: i32) -> Self {
     Self { quantity }
   }
 
@@ -41,8 +56,11 @@ impl Quantity {
   /// # Return
   ///
   /// * `Quantity`
-  pub fn add(self, quantity: u16) -> Self {
-    Quantity::new(self.quantity + quantity)
+  pub fn add(&self, value: i32) -> Result<Self, QuantityError> {
+    if value <= 0 {
+      Err(QuantityError)?
+    }
+    Ok(Quantity::new(self.quantity + value))
   }
 
   /// `quantity`を返却します
@@ -50,8 +68,46 @@ impl Quantity {
   /// # Return
   ///
   /// * `u16`
-  pub fn value(&self) -> u16 {
+  pub fn value(&self) -> i32 {
     self.quantity
   }
 }
 
+#[cfg(test)]
+mod tests {
+  use rstest::rstest;
+  use super::*;
+
+  #[rstest]
+  #[case(1, 10)]
+  #[case(2, 78)]
+  fn test_quantity_add_success(#[case] base: i32, #[case] value: i32) {
+    let quantity = Quantity::try_from(base);
+    let result = Quantity::add(&quantity.clone().unwrap(), value);
+
+    // assert
+    assert!(result.is_ok());
+    assert_eq!(base + value, result.unwrap().quantity)
+  }
+
+  #[rstest]
+  #[case(0)]
+  #[case(-1)]
+  fn test_quantity_add_failed(#[case] value: i32) {
+    let quantity = Quantity::try_from(1);
+    let result = Quantity::add(&quantity.unwrap(), value);
+
+    // assert
+    assert!(result.is_err())
+  }
+
+  #[rstest]
+  #[case(10)]
+  #[case(1)]
+  fn test_quantity_value_success(#[case] value: i32) {
+    let result = Quantity::try_from(value).unwrap().value();
+
+    // assert
+    assert_eq!(value, result)
+  }
+}
